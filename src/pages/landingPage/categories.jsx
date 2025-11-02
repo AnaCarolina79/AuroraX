bdc63e54f6654eae8804e8d6b24e82992367d858
AnaCarolina079 <anacarolinamoreiraluna@gmail.com>
2025-11-02 16:48:37 -0300

    .

--- src/pages/landingPage/categories.jsx
+++ src/pages/landingPage/categories.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-
+import './style.scss'
+import { FaSitemap } from 'react-icons/fa';
 
 function Categories() {
 const card = [
-    {id:1,text:'Desenvolvimento Web'},{id:2,text:'Desenvolvimento Mobile'},
-    {id:3,text:'Banco de Dados'},{id:4,text:'Machine Learning'},
-    {id:5,text:'Segurança da Informação'},{id:6,text:'Game Development'},
-    {id:7,text:'UX/UI Design'},{id:8,text:'Animação Digital'},
-    {id:9,text:'Modelagem e Arte 3D'},{id:10,text:'Edição de Vídeo'},
-    {id:11,text:'Ilustração Digital'},{id:11,text:'Motion Graphics'}];
+    {id:1,text:'Desenvolvimento Web',icon:<FaSitemap size={28}/>},{id:2,text:'Desenvolvimento Mobile',icon:<FaSitemap size={28}/>},
+    {id:3,text:'Banco de Dados',icon:<FaSitemap size={28}/>},{id:4,text:'Machine Learning',icon:<FaSitemap size={28}/>},
+    {id:5,text:'Inteligência Artificial',icon:<FaSitemap size={28}/>},{id:6,text:'Game Development',icon:<FaSitemap size={28}/>},
+    {id:7,text:'UX/UI Design',icon:<FaSitemap size={28}/>},{id:8,text:'Animação Digital',icon:<FaSitemap size={28}/>},
+    {id:9,text:'Modelagem e Arte 3D',icon:<FaSitemap size={28}/>},{id:10,text:'Edição de Vídeo',icon:<FaSitemap size={28}/>},
+    {id:11,text:'Ilustração Digital',icon:<FaSitemap size={28}/>},{id:11,text:'Motion Graphics', icon:<FaSitemap size={28}/>}];
    
     return (
         <>
-    {card.map((card) => <div key={card.id}>{card.text}</div>)}
+        <div className='categories'>
+
+        <h2>Nossos Tópicos</h2>
+        <div className='categorie'>
+    {card.map((card) => <div className='cards' key={card.id}>{card.icon}<p>
+        {card.text}</p></div>)}
+        </div>
+        </div>
         
         </>
   )
